5ecbef3b50308ff70d74595106473337a8711a0c
Reiii3 <rizamaulanaa3@gmail.com>
2025-09-09 23:24:04 +0700

    commit kedua penambahan awal structur web

---
+++ webroot/js/index.js
@@ -0,0 +1,27 @@
+class Main {
+    constructor() {
+        console.log('Hello Form AxPlugin Store');
+        this.initDocumnet();
+        this.initListener();
+    }
+
+    initListener() {
+        this.idBtnShowMenuNavigasi.addEventListener('click', () => {
+            this.blacktouchArea.style.display = 'block';
+            this.idLayoutNavigasi.style.transform = 'translateX(0)';
+        });
+        this.blacktouchArea.addEventListener('click', () => {
+            this.blacktouchArea.style.display = 'none';
+            this.idLayoutNavigasi.style.transform = 'translateX(-100%)';
+        });
+    }
+
+    initDocumnet() {
+        this.blacktouchArea = document.getElementById('black-touch');
+        this.idBtnShowMenuNavigasi = document.getElementById('btn-show-menu');
+        this.idLayoutNavigasi = document.getElementById('list-navigasi');
+        this.initListener();
+    }
+}
+
+new Main();
